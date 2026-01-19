9b8b0169abfa3d78da7e2d82e5b0f5e1bb0bbf81
Tuples <93516054+ttuples@users.noreply.github.com>
2026-01-19 05:40:06 -0500

    hytale permissions

--- src/main/java/dev/tuples/simplecaptcha/command/ShowCaptchaCommand.java
+++ src/main/java/dev/tuples/simplecaptcha/command/ShowCaptchaCommand.java
@@ -2,9 +2,11 @@ package dev.tuples.simplecaptcha.command;
 
 import com.hypixel.hytale.component.Ref;
 import com.hypixel.hytale.component.Store;
+import com.hypixel.hytale.server.core.Message;
 import com.hypixel.hytale.server.core.command.system.CommandContext;
 import com.hypixel.hytale.server.core.command.system.CommandSender;
 import com.hypixel.hytale.server.core.command.system.arguments.system.OptionalArg;
+import com.hypixel.hytale.server.core.command.system.arguments.system.RequiredArg;
 import com.hypixel.hytale.server.core.command.system.arguments.types.ArgTypes;
 import com.hypixel.hytale.server.core.command.system.basecommands.AbstractAsyncCommand;
 import com.hypixel.hytale.server.core.entity.entities.Player;
@@ -12,19 +14,17 @@ import com.hypixel.hytale.server.core.universe.PlayerRef;
 import com.hypixel.hytale.server.core.universe.world.World;
 import com.hypixel.hytale.server.core.universe.world.storage.EntityStore;
 import dev.tuples.simplecaptcha.gui.CaptchaPage;
+import dev.tuples.simplecaptcha.util.PermissionList;
 
 import javax.annotation.Nonnull;
 import java.util.concurrent.CompletableFuture;
 
 public class ShowCaptchaCommand extends AbstractAsyncCommand {
-    private final OptionalArg<PlayerRef> targetPlayerArg;
-
     public ShowCaptchaCommand() {
         super("simplecaptcha", "captcha.command.desc");
         this.addAliases("captcha");
-        this.setPermissionGroups("OP");
-
-        targetPlayerArg = withOptionalArg("player", "captcha.command.player", ArgTypes.PLAYER_REF);
+        this.requirePermission(PermissionList.USE.getPermission());
+        this.addSubCommand(new TargetCaptchaCommand());
     }
 
     private CompletableFuture<Void> openCaptchaPageAsync(Ref<EntityStore> ref) {
@@ -50,13 +50,34 @@ public class ShowCaptchaCommand extends AbstractAsyncCommand {
         CommandSender sender = context.sender();
 
         if (sender instanceof Player player) {
-            Ref<EntityStore> ref = targetPlayerArg.provided(context) ? targetPlayerArg.get(context).getReference() : player.getReference();
-            return openCaptchaPageAsync(ref);
+            return openCaptchaPageAsync(player.getReference());
         } else {
-            if (targetPlayerArg.provided(context)) {
-                return openCaptchaPageAsync(targetPlayerArg.get(context).getReference());
-            }
+            sender.sendMessage(Message.raw("This command can only be executed by a player."));
             return CompletableFuture.completedFuture(null);
+        }
+    }
+
+    private class TargetCaptchaCommand extends AbstractAsyncCommand {
+        private final RequiredArg<PlayerRef> targetPlayerArg;
+
+        public TargetCaptchaCommand() {
+            super("target", "captcha.command.target.desc");
+            this.requirePermission(PermissionList.TARGET.getPermission());
+            this.targetPlayerArg = withRequiredArg("player", "captcha.command.target.player", ArgTypes.PLAYER_REF);
+        }
+
+        @Nonnull
+        @Override
+        protected CompletableFuture<Void> executeAsync(CommandContext context) {
+            CommandSender sender = context.sender();
+
+            if (sender.hasPermission(PermissionList.TARGET.getPermission())) {
+                Ref<EntityStore> ref = targetPlayerArg.get(context).getReference();
+                return openCaptchaPageAsync(ref);
+            } else {
+                sender.sendMessage(PermissionList.TARGET.getMessage());
+                return CompletableFuture.completedFuture(null);
+            }
         }
     }
 }
---
+++ src/main/java/dev/tuples/simplecaptcha/util/PermissionList.java
@@ -0,0 +1,24 @@
+package dev.tuples.simplecaptcha.util;
+
+import com.hypixel.hytale.server.core.Message;
+import com.hypixel.hytale.server.core.entity.entities.Player;
+
+public enum PermissionList {
+    USE("use", "You do not have permission to use this command."),
+    TARGET("target", "You do not have permission to target other players with this command.");
+    private final String permission;
+    private final String denyMessage;
+
+    private PermissionList(String permission, String denyMessage){
+        this.permission = String.format("simplecaptcha.%s", permission);
+        this.denyMessage = denyMessage;
+    }
+
+    public Message getMessage(){
+        return Message.raw(denyMessage);
+    }
+
+    public String getPermission() {
+        return permission;
+    }
+}
